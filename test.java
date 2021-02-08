4d19faccf5f18053aafd778b4ec206c514e888db
srinivasanrd <78433541+srinivasanrd@users.noreply.github.com>
2021-02-08 13:18:06 +0000

    Update test.java

--- test.java
+++ test.java
@@ -1,4 +1,6 @@
-public static void main(String []args) {
+public class test
+{      
+  public static void main(String []args) {
       System.out.println("Hello World"); // prints Hello World
    }
 }
